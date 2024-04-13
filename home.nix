4aae72ba63cbb7bc11ac34fd07cc71a20cedea24
Jerry Wohlgemuth <wohlgejm@gmail.com>
2024-04-13 09:14:39 -0600

    Yanky view history

--- home.nix
+++ home.nix
@@ -79,6 +79,7 @@
       vim.keymap.set({"n","x"}, "p", "<Plug>(YankyPutAfter)")
       vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)")
       require("telescope").load_extension("yank_history")
+      vim.keymap.set("n", "<leader>fc", "<cmd>Telescope yank_history<cr>", {})
 
       local lspconfig = require('lspconfig')
       lspconfig.solargraph.setup {}
